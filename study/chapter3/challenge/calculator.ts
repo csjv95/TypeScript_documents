4c4b05397a4ee1d3377b6e332e6ff1861616fe61
csjv95 <csjv95@gmail.com>
2021-10-23 17:40:42 +0900

    challenge : calculate

---
+++ study/chapter3/challenge/calculator.ts
@@ -0,0 +1,29 @@
+/**
+ * Let's make a calculator 🧮
+ */
+type Type = "add" | "substract" | "multiply" | "divide" | "remainder";
+
+const calculate = (type: Type, x: number, y: number): number => {
+  switch (type) {
+    case "add":
+      return x + y;
+    case "substract":
+      return x - y;
+    case "multiply":
+      return x * y;
+    case "divide":
+      return x / y;
+    case "remainder":
+      return x % y;
+    default:
+      throw Error(
+        `Sorry.. you can choose one "add","substract","multiply","divide","remainder"`
+      );
+  }
+};
+
+console.log(calculate("add", 1, 3)); // 4
+console.log(calculate("substract", 3, 1)); // 2
+console.log(calculate("multiply", 4, 2)); // 8
+console.log(calculate("divide", 4, 2)); // 2
+console.log(calculate("remainder", 5, 2)); // 1
