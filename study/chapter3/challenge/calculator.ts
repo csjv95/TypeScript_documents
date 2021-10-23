7283418bc459c559966293ab59e25b9b20ddd8e3
csjv95 <csjv95@gmail.com>
2021-10-23 18:16:46 +0900

    Challenge : loading

--- study/chapter3/challenge/calculator.ts
+++ study/chapter3/challenge/calculator.ts
@@ -1,28 +1,30 @@
-// make a calculator 🧮
+{
+  // make a calculator 🧮
 
-type Type = "add" | "substract" | "multiply" | "divide" | "remainder";
+  type Type = "add" | "substract" | "multiply" | "divide" | "remainder";
 
-const calculate = (type: Type, x: number, y: number): number => {
-  switch (type) {
-    case "add":
-      return x + y;
-    case "substract":
-      return x - y;
-    case "multiply":
-      return x * y;
-    case "divide":
-      return x / y;
-    case "remainder":
-      return x % y;
-    default:
-      throw new Error(
-        `Sorry.. you can choose one "add","substract","multiply","divide","remainder"`
-      );
-  }
-};
+  const calculate = (type: Type, x: number, y: number): number => {
+    switch (type) {
+      case "add":
+        return x + y;
+      case "substract":
+        return x - y;
+      case "multiply":
+        return x * y;
+      case "divide":
+        return x / y;
+      case "remainder":
+        return x % y;
+      default:
+        throw new Error(
+          `Sorry.. you can choose one "add","substract","multiply","divide","remainder"`
+        );
+    }
+  };
 
-console.log(calculate("add", 1, 3)); // 4
-console.log(calculate("substract", 3, 1)); // 2
-console.log(calculate("multiply", 4, 2)); // 8
-console.log(calculate("divide", 4, 2)); // 2
-console.log(calculate("remainder", 5, 2)); // 1
+  console.log(calculate("add", 1, 3)); // 4
+  console.log(calculate("substract", 3, 1)); // 2
+  console.log(calculate("multiply", 4, 2)); // 8
+  console.log(calculate("divide", 4, 2)); // 2
+  console.log(calculate("remainder", 5, 2)); // 1
+}
--- study/chapter3/challenge/game.ts
+++ study/chapter3/challenge/game.ts
@@ -1,35 +1,37 @@
-// make a game 🕹
+{
+  // make a game 🕹
 
-import { error } from "console";
+  type Action = "up" | "down" | "left" | "right";
 
-type Action = "up" | "down" | "left" | "right";
+  const position = { x: 0, y: 0 };
+  const move = (action: Action) => {
+    switch (action) {
+      case "up":
+        position.x += 1;
+        break;
+      case "down":
+        position.x -= 1;
+        break;
+      case "left":
+        position.y -= 1;
+        break;
+      case "right":
+        position.y += 1;
+        break;
+      default:
+        throw new Error(
+          `Sorry.. you can choose one "up","down","left","right"`
+        );
+    }
+  };
 
-const position = { x: 0, y: 0 };
-const move = (action: Action) => {
-  switch (action) {
-    case "up":
-      position.x += 1;
-      break;
-    case "down":
-      position.x -= 1;
-      break;
-    case "left":
-      position.y -= 1;
-      break;
-    case "right":
-      position.y += 1;
-      break;
-    default:
-      throw new Error(`Sorry.. you can choose one "up","down","left","right"`);
-  }
-};
-
-console.log(position); // { x: 0, y: 0}
-move("up");
-console.log(position); // { x: 0, y: 1}
-move("down");
-console.log(position); // { x: 0, y: 0}
-move("left");
-console.log(position); // { x: -1, y: 0}
-move("right");
-console.log(position); // { x: 0, y: 0}
+  console.log(position); // { x: 0, y: 0}
+  move("up");
+  console.log(position); // { x: 0, y: 1}
+  move("down");
+  console.log(position); // { x: 0, y: 0}
+  move("left");
+  console.log(position); // { x: -1, y: 0}
+  move("right");
+  console.log(position); // { x: 0, y: 0}
+}
