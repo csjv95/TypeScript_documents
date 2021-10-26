ecf97932fe46117a1df69f88c04af28bc046bbd4
csjv95 <csjv95@gmail.com>
2021-10-26 13:52:54 +0900

    More clear folder and study OOP

---
+++ study/OOP/without_OOP/makeCoffee.ts
@@ -0,0 +1,25 @@
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
+
+  const BEANS_GRAM_PER_SHOT: number = 7;
+  let coffeeBeans: number = 0;
+
+  const makeCoffe = (shots: number): CoffeeCup => {
+    if (coffeeBeans < shots * BEANS_GRAM_PER_SHOT) {
+      throw new Error("Not enough coffee beans");
+    } else {
+      coffeeBeans -= shots * BEANS_GRAM_PER_SHOT;
+      return {
+        shots,
+        hasMilk: false,
+      };
+    }
+  };
+  coffeeBeans += 3 * BEANS_GRAM_PER_SHOT;
+
+  const coffee = makeCoffe(2);
+  console.log(coffee);
+}
