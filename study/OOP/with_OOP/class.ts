8b6f554adcbb0c56d9a4cca22bd7534b449d89c1
csjv95 <csjv95@gmail.com>
2021-10-26 15:13:27 +0900

    Study : making CoffeeMaker class

---
+++ study/OOP/with_OOP/class.ts
@@ -0,0 +1,28 @@
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
+
+  class CoffeeMaker {
+    static BEANS_GRAM_PER_SHOT: number = 7; // class level
+    coffeeBeans: number = 0;
+
+    constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    makeCoffe = (shots: number): CoffeeCup => {
+      if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAM_PER_SHOT) {
+        throw new Error("Not enough coffee beans");
+      }
+      this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAM_PER_SHOT;
+      return {
+        shots,
+        hasMilk: false,
+      };
+    };
+  }
+  const maker = new CoffeeMaker(32);
+  console.log(maker);
+}
