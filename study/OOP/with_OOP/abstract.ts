07d26dfa14bf6aba4c68b948f3fef81e18080a27
csjv95 <csjv95@gmail.com>
2021-10-27 17:25:27 +0900

    Study : abstract

---
+++ study/OOP/with_OOP/abstract.ts
@@ -0,0 +1,96 @@
+{
+  //abstract
+  // abstract 클래스로 만들어진 클래스는 자체적으로 object를 생성 할수없는 추상적인 클래스 입니다.
+  // 공통적인 기능들을 구현할 수 있습니다.
+  // 구현하는 클래스마다 달라야져야 하는 내용이 있으면 그부분만 abstract로 정의할 수 있습니다.
+  // 함수의 이름은 뭔지 언떤 인자를 받아 리턴하는지만 정의할 수있습니다
+  // 내부에서 쓰이는건 private로 외부에서 쓰이는건 publick으로
+  // abstract를 사용하면 더 안전하게 의도한대로 공통적인 기능들을 수행하며 달라진 부분만 상속클래스에게 꼭 구현하라고 강조할수 있습니다
+
+  type CoffeeCup = {
+    shots: number;
+    hasMilk?: boolean;
+    hasSugar?: boolean;
+  };
+
+  interface CoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+  }
+
+  abstract class CoffeeMachine implements CoffeeMaker {
+    private static BEANS_GRAMM_PER_SHOT: number = 7; // class level
+    private coffeeBeans: number = 0; // instance (object) level
+
+    constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    fillCoffeeBeans(beans: number) {
+      if (beans < 0) {
+        throw new Error("value for beans should be greater than 0");
+      }
+      this.coffeeBeans += beans;
+    }
+
+    clean() {
+      console.log("cleaning the machine...🧼");
+    }
+
+    private grindBeans(shots: number) {
+      console.log(`grinding beans for ${shots}`);
+      if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
+        throw new Error("Not enough coffee beans!");
+      }
+      this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
+    }
+
+    private preheat(): void {
+      console.log("heating up... 🔥");
+    }
+
+    protected abstract extract(shots: number): CoffeeCup;
+
+    makeCoffee(shots: number): CoffeeCup {
+      this.grindBeans(shots);
+      this.preheat();
+      return this.extract(shots);
+    }
+  }
+
+  class CaffeLatteMachine extends CoffeeMachine {
+    constructor(beans: number, public readonly serialNumber: string) {
+      super(beans);
+    }
+    private steamMilk(): void {
+      console.log("Steaming some milk... 🥛");
+    }
+
+    protected extract(shots: number): CoffeeCup {
+      this.steamMilk();
+      return {
+        shots,
+        hasMilk: true,
+      };
+    }
+  }
+
+  class SweetCoffeeMaker extends CoffeeMachine {
+    protected extract(shots: number): CoffeeCup {
+      return {
+        shots,
+        hasSugar: true,
+      };
+    }
+  }
+
+  const machines: CoffeeMaker[] = [
+    new CaffeLatteMachine(16, "1"),
+    new SweetCoffeeMaker(16),
+    new CaffeLatteMachine(16, "1"),
+    new SweetCoffeeMaker(16),
+  ];
+  machines.forEach((machine) => {
+    console.log("-------------------------");
+    machine.makeCoffee(1);
+  });
+}
